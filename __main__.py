6147fae6ff22fc2fb7a7fa16854a126045f13f5a
FrostSI <lovesi@126.com>
2023-12-16 02:39:33 +0800

    加入了更多log信息

--- __main__.py
+++ __main__.py
@@ -47,13 +47,15 @@ while True:
         continue
     if new_message.Name:
         wx.SwitchToThisWindow()
+        print('接收到新消息')
         new_message.Click(simulateMove=False)
         message_sender = wx.ButtonControl(Name='聊天信息').GetParentControl().GetParentControl().GetParentControl().TextControl().Name
+        print('消息发送者: ' + message_sender)
         if message_sender.find('(') and message_sender.find(')'):
             message_sender = message_sender[:message_sender.find('(')-1]
         if message_sender in allowed_senders:
             message = wx.ListControl(Name='消息').GetChildren()[-1].Name
-            print('sender: ' + message_sender + 'message: ' + message)
+            print('发送者: ' + message_sender + '消息: ' + message)
             if allowed_senders[message_sender][0] == 'individual':
                 Reply_ChatGPT(message)
             elif allowed_senders[message_sender][0] == 'group':
