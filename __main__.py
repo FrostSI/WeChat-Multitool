bf9ea6fa905ba7d48a921a25bff912f224675201
FrostSI <lovesi@126.com>
2023-12-16 17:45:42 +0800

    更新了log机制，修复了可能存在的@判断问题

--- __main__.py
+++ __main__.py
@@ -1,13 +1,42 @@
 import time
+from datetime import datetime
 import uiautomation
 from openai import OpenAI
 
 wx = uiautomation.WindowControl(ClassName='WeChatMainWndForPC')
 session = wx.ListControl(Name='会话')
 client = OpenAI(api_key = open('./API_KEY.txt').readline())
+log_file = open('./log.txt', 'a')
 me = ''
 admin = []
 
+def Reply_ChatGPT(sender, question_message):
+    success = False
+    for tries in range(3):
+        try:
+            answer_message = client.chat.completions.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": question_message}]).choices[0].message.content
+        except:
+            Log(sender + ' ' + str(tries + 1) + ' attempt failed')
+            continue
+        else:
+            wx.SwitchToThisWindow()
+            wx.SendKeys(text=answer_message, waitTime=0)
+            wx.SendKeys('{Enter}', waitTime=0)
+            success = True
+            Log(sender + ' question answered')
+            break
+    if not success:
+        wx.SwitchToThisWindow()
+        wx.SendKeys(text='连接错误', waitTime=0)
+        wx.SendKeys('{Enter}', waitTime=0)
+        Log(sender + ' question connection failed')
+
+def Log(log):
+    log = str(log)
+    print(log)
+    log_file.write(log + '\n')
+
+Log(datetime.now())
 lines = open('./User_List.txt', encoding = 'utf-8').readlines()
 allowed_senders = {}
 for line in lines:
@@ -19,27 +48,6 @@ for line in lines:
         admin.append(line_content[0])
     allowed_senders[line_content[0]] = [line_content[1], line_content[2]]
 
-print(allowed_senders)
-
-def Reply_ChatGPT(question_message):
-    success = False
-    for tries in range(3):
-        try:
-            answer_message = client.chat.completions.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": question_message}]).choices[0].message.content
-        except:
-            print(message_sender + ' ' + str(tries + 1) + ' attempt failed')
-            continue
-        else:
-            wx.SwitchToThisWindow()
-            wx.SendKeys(text=answer_message, waitTime=0)
-            wx.SendKeys('{Enter}', waitTime=0)
-            success = True
-            break
-    if not success:
-        wx.SwitchToThisWindow()
-        wx.SendKeys(text='连接错误', waitTime=0)
-        wx.SendKeys('{Enter}', waitTime=0)
-
 while True:
     time.sleep(1)
     new_message = session.TextControl(searchDepth=4)
@@ -47,19 +55,16 @@ while True:
         continue
     if new_message.Name:
         wx.SwitchToThisWindow()
-        print('接收到新消息')
         new_message.Click(simulateMove=False)
         message_sender = wx.ButtonControl(Name='聊天信息').GetParentControl().GetParentControl().GetParentControl().TextControl().Name
         if '(' in message_sender and ')' in message_sender:
             message_sender = message_sender[:message_sender.find('(')-1]
-        print('消息发送者: ' + message_sender)
-        print(allowed_senders)
         if message_sender in allowed_senders:
             message = wx.ListControl(Name='消息').GetChildren()[-1].Name
-            print('发送者: ' + message_sender + '消息: ' + message)
+            Log('发送者: ' + message_sender + ' | 消息: ' + message)
             if allowed_senders[message_sender][0] == 'individual':
-                Reply_ChatGPT(message)
+                Reply_ChatGPT(message_sender, message)
             elif allowed_senders[message_sender][0] == 'group':
                 prefix = '@' + me + ' '
-                if message.find(prefix):
-                    Reply_ChatGPT(message)
+                if prefix in message:
+                    Reply_ChatGPT(message_sender, message)
